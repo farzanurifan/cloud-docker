fe1716ce27dc17655d6210af8b986e0e566e6a37
Farza Nurifan <farzanurifan@gmail.com>
2018-12-11 07:54:25 +0700

    Fix cookies name

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.static(__dirname + '/data'))
 // Storage config
 var Storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        var dir = req.cookies.login_dir
+        var dir = req.cookies.cloud_dir
         callback(null, dir)
     },
     filename: (req, file, callback) => {
@@ -84,30 +84,30 @@ app.listen(3000, () => console.log('listening on 3000'))
 
 // Routing //
 app.get('/login', (req, res) => {
-    if (req.cookies.login_cloud) return res.redirect('/')
+    if (req.cookies.cloud_login) return res.redirect('/')
     res.render('login.ejs', { loggedInStatus: 'Not logged in' })
 })
 
 app.get('/logout', (req, res) => {
-    res.clearCookie('login_cloud')
-    res.clearCookie('login_dir')
-    res.clearCookie('login_username')
-    res.clearCookie('login_id')
+    res.clearCookie('cloud_login')
+    res.clearCookie('cloud_dir')
+    res.clearCookie('cloud_username')
+    res.clearCookie('cloud_id')
     res.redirect('/')
 })
 
 app.get('/register', (req, res) => {
-    if (req.cookies.login_cloud) return res.redirect('/')
+    if (req.cookies.cloud_login) return res.redirect('/')
     res.render('register.ejs', { loggedInStatus: 'Not logged in' })
 })
 
 app.get('/', (req, res) => res.redirect('/page/1'))
 
 app.get('/page/:page', (req, res) => {
-    if (!req.cookies.login_cloud) res.redirect('/login')
-    var dir = req.cookies.login_dir
-    var loggedInStatus = `Logged in as ${req.cookies.login_username}`
-    var username = req.cookies.login_username
+    if (!req.cookies.cloud_login) res.redirect('/login')
+    var dir = req.cookies.cloud_dir
+    var loggedInStatus = `Logged in as ${req.cookies.cloud_username}`
+    var username = req.cookies.cloud_username
     var page = Number(req.params.page)
     let results = []
     let stats = null
@@ -144,14 +144,14 @@ app.get('/page/:page', (req, res) => {
 })
 
 app.get('/add', (req, res) => {
-    if (!req.cookies.login_cloud) res.redirect('/login')
-    var loggedInStatus = `Logged in as ${req.cookies.login_username}`
+    if (!req.cookies.cloud_login) res.redirect('/login')
+    var loggedInStatus = `Logged in as ${req.cookies.cloud_username}`
     res.render('add.ejs', { fields, loggedInStatus })
 })
 
 app.get('/download/:filename', (req, res) => {
-    if (!req.cookies.login_cloud) res.redirect('/login')
-    var dir = req.cookies.login_dir
+    if (!req.cookies.cloud_login) res.redirect('/login')
+    var dir = req.cookies.cloud_dir
     var filename = req.params.filename
 //    console.log(`${filename} downloaded`)
     var file = `${dir}/${filename}`
@@ -159,17 +159,17 @@ app.get('/download/:filename', (req, res) => {
 })
 
 app.get('/edit/:filename', (req, res) => {
-    if (!req.cookies.login_cloud) res.redirect('/login')
+    if (!req.cookies.cloud_login) res.redirect('/login')
     var filename = req.params.filename
-    var loggedInStatus = `Logged in as ${req.cookies.login_username}`
+    var loggedInStatus = `Logged in as ${req.cookies.cloud_username}`
     res.render('edit.ejs', { result: { filename }, fields: ['filename'], loggedInStatus })
 })
 
 app.get('/view/:filename', (req, res) => {
-    if (!req.cookies.login_cloud) res.redirect('/login')
-    var idUser = req.cookies.login_id
+    if (!req.cookies.cloud_login) res.redirect('/login')
+    var idUser = req.cookies.cloud_id
     var filename = req.params.filename
-    var loggedInStatus = `Logged in as ${req.cookies.login_username}`
+    var loggedInStatus = `Logged in as ${req.cookies.cloud_username}`
     res.render('view.ejs', { result: { filename }, fields: ['filename'], loggedInStatus, filename, idUser })
 })
 
@@ -193,10 +193,10 @@ app.post('/api/login', (req, res) => {
             loggedInStatus = `Logged in as ${result.name}`
             dir = `./data/${result._id}`
 //            console.log(`logged in as ${result.name}`)
-            res.cookie('login_cloud', true, options)
-            res.cookie('login_dir', dir, options)
-            res.cookie('login_username', result.name, options)
-            res.cookie('login_id', result._id, options)
+            res.cookie('cloud_login', true, options)
+            res.cookie('cloud_dir', dir, options)
+            res.cookie('cloud_username', result.name, options)
+            res.cookie('cloud_id', result._id, options)
         }
         else console.log('failed to log in')
         res.redirect('/')
@@ -230,7 +230,7 @@ app.get('/api/size/:name', (req, res) => {
 })
 
 app.post('/api/upload', (req, res) => {
-    var username = req.cookies.login_username
+    var username = req.cookies.cloud_username
     request({
         method: 'GET',
         url: `http://127.0.0.1:3000/api/size/${username}`,
